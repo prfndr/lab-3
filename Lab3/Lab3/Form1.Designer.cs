449a3f250a4c5d053c78a0710de5a782350f969b
prfndr <74804416+prfndr@users.noreply.github.com>
2021-01-26 04:04:11 +0500

    Add files via upload

---
+++ Lab3/Lab3/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBuildChart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(46, 42);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(313, 27);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(133, 103);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(133, 148);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "X2";
+            // 
+            // btnBuildChart
+            // 
+            this.btnBuildChart.Location = new System.Drawing.Point(133, 258);
+            this.btnBuildChart.Name = "btnBuildChart";
+            this.btnBuildChart.Size = new System.Drawing.Size(134, 49);
+            this.btnBuildChart.TabIndex = 5;
+            this.btnBuildChart.Text = "Построить график";
+            this.btnBuildChart.UseVisualStyleBackColor = true;
+            this.btnBuildChart.Click += new System.EventHandler(this.btnBuildChart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 362);
+            this.Controls.Add(this.btnBuildChart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBuildChart;
+    }
+}
+
---
+++ Lab3/Lab3/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        dynamic xlApp;
+        dynamic xlWorksheet;
+        dynamic xlRange;
+        Type typeExcel;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadExcel();
+
+            for (int i = 1; i < 5; i++)
+            {
+                string funcName = xlRange.Cells[i, 1].Value2;
+                comboBox1.Items.Add(funcName);
+            }
+            xlApp.Quit();
+        }
+
+        private void LoadExcel()
+        {
+            typeExcel = Type.GetTypeFromProgID("Excel.Application");
+            xlApp = Activator.CreateInstance(typeExcel);
+            dynamic xlWorkbook = xlApp.Workbooks.Open(Application.StartupPath + "\\Lab3.1.xlsm");
+            xlWorksheet = xlWorkbook.Sheets[1];
+            xlRange = xlWorksheet.UsedRange;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            
+        }
+
+        private void btnBuildChart_Click(object sender, EventArgs e)
+        {
+            LoadExcel();
+
+            int typeFunc = comboBox1.SelectedIndex + 1;
+
+            xlRange.Cells[2, 6].Value2 = typeFunc;
+            
+
+            int x1 = (int)numericUpDown1.Value;
+            int x2 = (int)numericUpDown2.Value;
+
+            int i = 10;
+
+            for (int x = x1; x <= x2; x++)
+            {
+                xlRange.Cells[3, 6].Value2 = x;
+                double y = xlRange.Cells[6, 6].Value2;
+
+                xlRange.Cells[i, 2].Value2 = x;
+                xlRange.Cells[i, 3].Value2 = y;
+                i++;
+            }
+
+            dynamic shape = xlWorksheet.Shapes.AddChart2(240, Excel.XlChartType.xlXYScatterSmooth);
+            dynamic series = shape.Chart.SeriesCollection(1);
+            series.XValues = xlWorksheet.Range("B10", "B" + (i - 1));
+            series.Values = xlWorksheet.Range("C10", "C" + (i - 1));
+
+            //xlApp.Visible = true;
+            typeExcel.InvokeMember("Visible", BindingFlags.SetProperty, null, xlApp, new object[1] { true });
+        }
+    }
+}
